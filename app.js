863bf9547f9ec760033916397d96c534b0d8da74
Jeffmagma <jeffrey.magmachu@gmail.com>
2018-05-26 16:03:21 -0400

    i think i forgot to save before pushing?

--- app.js
+++ app.js
@@ -39,8 +39,27 @@ function createWindow() {
 				},
 				{type: 'separator'},
 				{
-					label: 'Kappa', click() {
-						console.log('Kappa clicked');
+					label: 'Open',
+					click() {
+						console.log('open clicked');
+					}
+				},
+				{
+					label: 'Save',
+					click() {
+						console.log('save clicked');
+					}
+				},
+				{
+					label: 'Save As',
+					click() {
+						console.log('save as clicked');
+					}
+				},
+				{
+					label: 'Print',
+					click() {
+						console.log('print clicked');
 					}
 				}
 			]
@@ -69,6 +88,98 @@ function createWindow() {
 						console.log('wtf does this do');
 					}
 				},
+			]
+		},
+		{
+			label: 'Settings',
+			submenu: [
+				{
+					label: 'Preferences',
+					click() {
+						console.log('preferences clicked');
+					}
+				},
+				{
+					label: 'Edit Assembler Environment',
+					click() {
+						console.log('edit assembler environment clicked');
+					}
+				},
+				{
+					label: 'Edit Runtime Environment',
+					click() {
+						console.log('edit runtime environment clicked');
+					}
+				}
+			]
+		},
+		{
+			label: 'Run',
+			submenu: [
+				{
+					label: 'Run All',
+					click() {
+						console.log('run all clicked');
+					}
+				},
+				{
+					label: 'Run Step-by-Step',
+					click() {
+						console.log('run step by step clicked');
+					}
+				},
+				{
+					label: 'Run to Breakpoint',
+					click() {
+						console.log('run to breakpoint clicked');
+					}
+				}
+			]
+		},
+		{
+			label: 'Sample Code',
+			submenu: [
+				{
+					label: 'Sample Code 1',
+					click() {
+						console.log('sample code 1 clicked');
+					}
+				},
+				{
+					label: 'Sample Code 2',
+					click() {
+						console.log('sample code 2 clicked');
+					}
+				},
+				{
+					label: 'Sample Code 3',
+					click() {
+						console.log('sample code 3 clicked');
+					}
+				}
+			]
+		},
+		{
+			label: 'Help',
+			submenu: [
+				{
+					label: '8085 Help',
+					click() {
+						console.log('8085 help clicked');
+					}
+				},
+				{
+					label: 'Editor Help',
+					click() {
+						console.log('editor help clicked');
+					}
+				},
+				{
+					label: 'About the Editor',
+					click() {
+						console.log('about the editor clicked');
+					}
+				}
 			]
 		}
 	];
