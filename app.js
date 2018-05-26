bf3590a471f883ba58c9f563cbcbb0dc676af77c
Jeffmagma <jeffrey.magmachu@gmail.com>
2018-05-26 15:44:30 -0400

    options all added, not tested

--- app.js
+++ app.js
@@ -48,15 +48,27 @@ function createWindow() {
 		{
 			label: 'Edit',
 			submenu: [
-				{role: 'undo'},
-				{role: 'redo'},
-				{type: 'separator'},
 				{role: 'cut'},
 				{role: 'copy'},
 				{role: 'paste'},
 				{role: 'pasteandmatchstyle'},
-				{role: 'delete'},
-				{role: 'selectall'}
+				{type: 'separator'},
+				{role: 'undo'},
+				{role: 'redo'},
+				{role: 'selectall'},
+				{type: 'separator'},
+				{
+					label: 'Auto-Indent',
+					click() {
+						console.log('auto indent clicked');
+					}
+				},
+				{
+					label: 'Show Mistakes',
+					click() {
+						console.log('wtf does this do');
+					}
+				},
 			]
 		}
 	];
