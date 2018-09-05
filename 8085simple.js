0de29c22baef2a7510d64827e556c1ea1d60bee0
Jeffmagma <jeffrey.magmachu@gmail.com>
2018-09-04 23:07:42 -0400

    implemented a few more keywords

--- 8085simple.js
+++ 8085simple.js
@@ -2,7 +2,8 @@ CodeMirror.defineSimpleMode("8085", {
 	// The start state contains the rules that are intially used
 	start: [
 		// The regex matches the token, the token property contains the type
-		{regex: /push|pop/, token: "keyword"},
+		{regex: /push|PUSH|pop|POP|nop|NOP|hlt|HLT/, token: "keyword"},
+		{regex: /di|DI|ei|EI|rim|RIM|sim|SIM/, token: "error"},
 	],
 	// The multi-line comment state.
 	comment: [
