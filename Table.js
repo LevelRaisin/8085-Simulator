7a5e9b20ac3a461ac6dea3fb384d9d758db71104
Liam Telenko <mailtelenko@gmail.com>
2018-09-10 12:42:00 -0400

    Fixed/finished tables
    - Removed old folders
    - Cleaned up CSS
    - Cleaned up JS
    - Finished implementing tables.

--- Table.js
+++ Table.js
@@ -1,27 +1,46 @@
 //Object to create a table
 function Table(tableName, tableData) {
+	//Variables
 	this.name = tableName;
 	this.data = tableData;
+	this.tableNumbers;
 	this.tableHeader = "";
 
-	this.updateData = function(row, col, newData) {
-		data[row][col] = newData; //Still needs development
+	//Update a cell of the table (not header)
+	this.updateData = function (row, col, newData) {
+		this.data[row][col] = newData; //Still needs development
 
-		this.updateTable();
-	}
-
-	this.updateTable = function(tableData) {
-		$(this.name).html(this.tableHeader + tableData);
-	}
-
-	this.createTable = function() {
-		this.tableHeader = this.createHeader(this.data[0]);
 		this.populateTable(this.data);
 	}
 
-	this.populateTable = function(tableData) {
+	//Update the data of the table (header + body data)
+	this.updateTable = function (tableData) {
+		$(this.name).html(this.tableHeader + tableData);
+	}
+
+	//Initially creates the table with passed in data (header + updateTable())
+	this.createTable = function (useData) {
+		this.tableHeader = this.createHeader(this.data[0]);
+		this.tableNumbers = false;
+
+		if (!useData) {
+			for (var y = 1; y < 10; y++) {
+				this.data[y] = [];
+				for (var x = 0; x < this.data[0].length - 1; x++)
+					this.data[y][x] = "00";
+			}
+			this.tableNumbers = true;
+		}
+
+		this.populateTable(this.data);
+	}
+
+	//Populates the data secion of the table
+	this.populateTable = function (tableData) {
 		var tablePopulation = "";
 		for (var x = 1; x < tableData.length; x++) {
+			if (this.tableNumbers == true)
+				tablePopulation += "<td>" + x.toString(2) + "</td>";
 			for (var z = 0; z < tableData[x].length; z++)
 				tablePopulation += "<td>" + tableData[x][z] + "</td>"
 			tablePopulation += "</tr>";
@@ -31,7 +50,7 @@ function Table(tableName, tableData) {
 	}
 
 	//Creates the header of the table
-	this.createHeader = function(headerData) {
+	this.createHeader = function (headerData) {
 		var returnData = "<!-- Table headings --><tr id='tableHeaders'>";
 
 		//Add every header to table
@@ -43,19 +62,19 @@ function Table(tableName, tableData) {
 
 	}
 
-	this.highlightMemory = function() {
+	//"Highlights" the important segments of data in the tables
+	this.highlightMemory = function () {
 		//Row counter
 		var rowCount = 0;
 
 		//Reset all rows to not active
-		$(this.name).find("tr").each(function() {
+		$(this.name).find("tr").each(function () {
 			$(this).removeClass('active');
 		});
 
 		//Add active class to rows/items with memory != 0
-		$(this.name).find("td").each(function() {
+		$(this.name).find("td").each(function () {
 			//Check if memory has a 1 and is of length 2 (to exclude line number)
-			console.log(this);
 			if (this.innerText.includes("1") && this.innerText.length == 2) {
 				$(this).toggleClass('active'); //Toggle active class
 				//Add active class if the memory location was toggled to active
@@ -63,18 +82,36 @@ function Table(tableName, tableData) {
 					$(this).closest("tr").addClass('active');
 			}
 		});
+	}
 
-
+	//Accessor method
+	this.getData = function () {
+		return this.data;
 	}
 }
+//
+//UI Elements
+//
 
-$(document).on("click", "#createTable", function() {
-	var test = new Table("#testTable", [["Col 1", "Col 2", "Col 3"], ["Row 1", "Row 1", "Row 1"]]);
+//New window button
+$('#popoutButton').click(function () {
+	const remote = require('electron').remote;
+	const path = require('path')
+	const BrowserWindow = remote.BrowserWindow;
+	const modalPath = path.join('file://', __dirname, 'popout.html')
 
-	test.createTable();
+	var win = new BrowserWindow({ width: 800, height: 600 });
+
+	win.loadURL(modalPath);
+
 });
 
-//Listen for click on show active memory
-$(document).on("click", "#showMemory", function() {
-	test.hightlightMemory();
+//Hightlight button
+$("#revealButton").click(function () {
+	if ($("#revealButton").html() == '<i class="material-icons">search</i>Reveal')
+		$("#revealButton").html('<i class="material-icons">search</i>Hide');
+	else
+		$("#revealButton").html('<i class="material-icons">search</i>Reveal');
+	memTable.highlightMemory();
+	binTable.highlightMemory();
 });
--- render.js
+++ render.js
@@ -1,5 +1,5 @@
 let $ = jQuery = require('jquery');
-let {remote} = require('electron');
+let { remote } = require('electron');
 //let jQueryui = require('jquery-ui');
 //let tabs = require('jquery-ui/ui/widgets/tabs');
 //let draggable = require('jquery-ui/ui/widgets/draggable');
@@ -42,7 +42,7 @@ function count_set_bits(i) {
 }
 
 let commands = {};
-commands["cmp"] = new command([0xb8, 0xbf], function(params) {
+commands["cmp"] = new command([0xb8, 0xbf], function (params) {
 	let temp = registers["a"] - registers[params[0]];
 	if (temp === 0) zero = 1;
 	else if (temp > 1) carry = 1;
@@ -51,7 +51,7 @@ commands["cmp"] = new command([0xb8, 0xbf], function(params) {
 		carry = 0;
 	}
 });
-commands["cpi"] = new command([0xfe], function(params) {
+commands["cpi"] = new command([0xfe], function (params) {
 	let temp = registers["a"] - params[0];
 	if (temp === 0) zero = 1;
 	if (temp > 1) carry = 1;
@@ -60,7 +60,7 @@ commands["cpi"] = new command([0xfe], function(params) {
 		carry = 0;
 	}
 });
-commands["ana"] = new command([0xa0, 0xa7], function(params) {
+commands["ana"] = new command([0xa0, 0xa7], function (params) {
 	registers["a"] = registers["a"] & registers[params[0]];
 	let a = registers["a"];
 	if (a < 0) sign = 1;
@@ -69,7 +69,7 @@ commands["ana"] = new command([0xa0, 0xa7], function(params) {
 	carry = 0;
 	auxiliary_carry = 1;
 });
-commands["ani"] = new command([0xe6], function(params) {
+commands["ani"] = new command([0xe6], function (params) {
 	registers["a"] = registers["a"] & params[0];
 	let a = registers["a"];
 	if (a < 0) sign = 1;
@@ -78,7 +78,7 @@ commands["ani"] = new command([0xe6], function(params) {
 	carry = 0;
 	auxiliary_carry = 1;
 });
-commands["xra"] = new command([0xa8, 0xaf], function(params) {
+commands["xra"] = new command([0xa8, 0xaf], function (params) {
 	registers["a"] = registers["a"] ^ registers[params[0]];
 	let a = registers["a"];
 	if (a < 0) sign = 1;
@@ -97,34 +97,34 @@ function run() {
 	$("#editor").css('display', 'none');
 	$("#tables").css('display', 'block');
 
-	memTable = new Table("#mem-table", [["Memory Locations", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"], ["00", "01", "10", "00", "00", "00", "01", "10", "00", "00"], ["00", "00", "00"], ["00", "01", "10", "00", "00"]]);
-	binTable = new Table("#bin-table", [["Memory Locations", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"], ["Test Data"]]);
+	memTable = new Table("#mem-table", [["Memory Locations", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]]);
+	binTable = new Table("#bin-table", [["Name", "Binary Representation", "Hexadecimal"],["A","0000 0000","00"], ["B","0000 0000","00"], ["C","0000 0000","00"], ["D","0000 0000","00"], ["E","0000 0000","00"], ["H","0000 0000","00"], ["L","0000 0000","00"], ["M","0000 0000","00"], ["PC","0000 0000 0000 0000","00 00"], ["SP","0000 0000 0000 0000","00 00"], ["Flags","0000 0000","00"]]);
 
 	memTable.createTable();
-	binTable.createTable();
+	binTable.createTable(true);
 
 	const lines = area.getValue().toLowerCase().split("\n");
-	console.log(table);
+	//console.log(table);
 	for (let i in lines) {
 		const cmds = lines[i].split(/[ ,]/);
-		console.log(cmds);
+		//console.log(cmds);
 		const cmd = cmds[0];
 		if (cmd === "hlt") break;
 		else if (cmd !== "nop") {
-			console.log(commands[cmd]);
+			//console.log(commands[cmd]);
 			commands[cmd].func(cmds.slice(1));
 		}
 	}
 }
 
-$(function() {
+$(function () {
 	const $project_explorer = $('#project_explorer');
 	const $tabs = $('#tabs');
 	const $separator = $('#h_sep');
 	const $run = $('#run');
 	$tabs.tabs();
 	$run.click(run);
-	const splitter = function(event, ui) {
+	const splitter = function (event, ui) {
 		ui.position.left = Math.max(window.innerWidth * .1, ui.position.left);
 		ui.position.left = Math.min(window.innerWidth * .9, ui.position.left);
 		const aw = ui.position.left, bw = window.innerWidth - 4 - aw;
